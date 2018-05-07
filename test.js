c0489fb3542abada1cc0981d7d01874ef5778c04
Code Nart <keinart7@gmail.com>
2018-05-08 02:43:34 +0700

    'patch'

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const Just = require('./index')
 const testIndex =
    () =>
       { let just = null
-        // --
+        //-
         just = Just()
         expect(just.value).toBe(null)
         expect(just.memory).toEqual([])
-        // --
+        //-
         just = just.be('word')
         expect(just.value).toBe('word')
         expect(just.memory).toEqual([ 'word' ])
