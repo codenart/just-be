a991733c52f4da36d87c9aa7c500f934e03b07ae
Kei Nart <keinart7@gmail.com>
2018-05-21 03:57:33 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
 const Just = require('./index')
 
-const testIndex =
-   () =>
-      { let just = null
-        //-
-        just = Just()
-        expect(just.value).toBe(null)
-        expect(just.memory).toEqual([])
-        //-
-        just = just.be('word')
-        expect(just.value).toBe('word')
-        expect(just.memory).toEqual([ 'word' ])
-      }
-
-test('just', testIndex)
+test('just', () => {
+   let just = null
+   //-
+   just = Just()
+   expect(just.value).toBe(null)
+   expect(just.memory).toEqual([])
+   //-
+   just = just.be('word')
+   expect(just.value).toBe('word')
+   expect(just.memory).toEqual([ 'word' ])
+})
